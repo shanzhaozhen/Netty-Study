1eec17fb0a9172e366fa9aae6289c810151e18f3
shanzhaozhen <204447118@qq.com>
2022-11-18 15:36:57 +0800

    update

---
+++ src/main/java/com/example/nettystudy/demo/README.md
@@ -0,0 +1,49 @@
+# Netty demos. （Netty 案例大全）
+
+Demos of [Netty 4.x User Guide](https://github.com/waylau/netty-4-user-guide) 《Netty 4.x 用户指南》/《Netty原理解析与开发实战》，文中用到的例子源码。
+
+
+## 版本
+
+涉及的相关技术及版本如下。
+
+* Netty 4.1.52.Final
+* Jackson 2.10.1
+* JUnit 5.5.2
+
+
+## 示例
+
+包含示例如下：
+
+* [Java标准I/O实现Echo服务器、客户端](net)
+* [Java NIO实现Echo服务器、客户端](nio)
+* [Java AIO实现Echo服务器、客户端](aio)
+* [Netty实现Echo服务器、客户端](netty/demo/echo)
+* [Netty实现丢弃服务器](netty/demo/discard)
+* [Netty实现时间服务器](netty/demo/time)
+* [Java ByteBuffer使用案例](buffer)
+* [Netty ByteBuf使用案例](netty/demo/buffer)
+* [Netty ByteBuf的三种使用模式](netty/demo/buffer)
+* [Netty实现无连接协议Echo服务器、客户端](netty/demo/echo)
+* [Java线程池示例](concurrent/ThreadPoolExecutorDemo.java)
+* [Java Reactor示例](reactor)
+* [自定义基于换行的解码器](netty/demo/decoder)
+* [TCP客户端](netty/TcpClient.java)
+* [自定义编码器](netty/demo/encoder)
+* [自定义编解码器](netty/demo/codec)
+* [实现心跳机制](netty/demo/heartbeat)
+* [基于Netty的对象序列化](netty/demo/codec/serialization)
+* [基于Jackson的JSON序列化](netty/demo/codec/jackcon)
+* [基于SSL/TSL的双向认证Echo服务器和客户端](netty/demo/secureecho)
+* [基于HTTP的Web服务器](netty/demo/httpserver)
+* [基于HTTP/2的Web服务器和客户端](netty/demo/http2)
+* [基于WebSocket的聊天室](netty/demo/websocketchat)
+* [lite-monitoring](https://github.com/waylau/lite-monitoring)
+* [lite-monitoring-ui](https://github.com/waylau/lite-monitoring-ui)
+* 陆续整理中...
+
+## 配套书籍
+
+* 开源书《[Netty 4.x User Guide](https://github.com/waylau/netty-4-user-guide)》
+* 正式出版物《[Netty原理解析与开发实战](https://book.douban.com/subject/35317298/)》
---
+++ src/main/java/com/example/nettystudy/demo/net/BlockingEchoClient.java
@@ -0,0 +1,50 @@
+/**
+ * Welcome to https://waylau.com
+ */
+package com.example.nettystudy.demo.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Blocking Echo Client.
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
+ * @since 1.0.0 2019年9月28日
+ */
+public class BlockingEchoClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String hostName = "127.0.0.1";
+        int portNumber = 7;
+
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            System.out.print("请输入:");
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+                System.out.print("请输入:");
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("不明主机，主机名为： " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("不能从主机中获取I/O，主机名为：" + hostName);
+            System.exit(1);
+        }
+    }
+
+}
